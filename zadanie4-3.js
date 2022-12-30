f257aa7b3f9d4208192c178c1010f14b163e756a
EvgenyGoryunov <ges1987@list.ru>
2022-12-31 00:13:09 +0700

    111

---
+++ zadanie4-3.js
@@ -0,0 +1,19 @@
+// Задание 3.
+
+// Написать функцию, которая создает пустой объект, но без прототипа.
+
+
+
+
+function test() {
+    return Object.create(null)
+}
+
+console.log(test())
+
+
+// Ошибка, кот возникает, если в скобках без (null)
+// zadanie4-3.js:2 Uncaught TypeError: Object prototype may only be an Object or null: undefined
+//     at Function.create (<anonymous>)
+//     at test (zadanie4-3.js:2:19)
+//     at zadanie4-3.js:5:13
